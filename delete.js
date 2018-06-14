67966f9d3859a95345e2aa908e6f18fa880285fd
dahuazheng <webdahua@163.com>
2018-06-14 16:30:30 +0800

    delete todos

--- delete.js
+++ delete.js
@@ -2,3 +2,11 @@ let mysql = require('mysql');
 let config = require('./config.js');
 
 let connection = mysql.createConnection(config);
+let sql = `DELETE FROM todos WHERE id = ?`;
+
+connection.query(sql, 1, (error, results, fields) => {
+    if (error) return console.error('error:' + error.message);
+    console.log('Delete Row(s):', results.affectedRows)
+});
+
+connection.end();
